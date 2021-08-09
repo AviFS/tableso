3f4450e56730baef1dd87da327be15a333ac70bf
AviFS <avifsternlieb@gmail.com>
2021-08-09 04:44:28 -0700

    The styling is getting there.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function run() {
     document.getElementById('out').innerHTML = '';
-    inp = document.getElementById('inp').value;
+    inp = document.getElementById('inp').innerText;
     var [output, s] = interp(inp);
     document.getElementById('out').innerHTML = output;
 }
